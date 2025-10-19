3a7efe41d46f4d11cafe963ffb188fcb29d9995f
Aleksi Alhola <aleksi.alhola@gmail.com>
2025-10-19 13:53:10 +0300

    emit response through event

--- api/index.js
+++ api/index.js
@@ -1,6 +1,30 @@
 const http = require("http");
 const url = require("url");
+const net = require("net");
+
 const PORT = process.env.PORT;
+const eventController = net.createServer();
+
+class EventQueue {
+  static responseMap = {};
+
+  static addRequestToQueue(id, res) {
+    EventQueue.responseMap[id] = res;
+  }
+
+  static onEventTriggered(e) {
+    console.log("Event triggered");
+    console.log(e);
+    const res = EventQueue.responseMap.TEMP_ID;
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify(e));
+    EventQueue.removeRequest("TEMP_ID");
+  }
+
+  static removeRequest(id) {
+    delete EventQueue.responseMap[id];
+  }
+}
 
 const server = http.createServer(async (req, res) => {
   const method = req.method;
@@ -11,10 +35,23 @@ const server = http.createServer(async (req, res) => {
     res.end("Not Found");
     return;
   }
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(JSON.stringify(parsedUrl.query));
+
+  EventQueue.addRequestToQueue("TEMP_ID", res);
+  await new Promise((res) => setTimeout(res, 1000));
+  eventController.emit("event", parsedUrl.query);
+  if (EventQueue.responseMap.TEMP_ID) {
+    console.log("Request timed out");
+    EventQueue.removeRequest("TEMP_ID");
+    res.writeHead(500);
+    res.end("Internal Server Error");
+  }
 });
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port: ${PORT}`);
+
+  eventController.listen("/tmp/eventController", () => {
+    console.log("Listening on events");
+  });
+  eventController.on("event", EventQueue.onEventTriggered);
 });
